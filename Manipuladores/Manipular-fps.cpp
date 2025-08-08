893465a5e8ee142b1ab7711abbe56c57df614c4f
Braga <204622249+braga2311@users.noreply.github.com>
2025-08-08 07:49:58 -0300

    coe kkkz so pega no FF msm, não testei em outros jogos mas a lógica e para funcionar apenas nele

--- Manipuladores/Manipular-fps.cpp
+++ Manipuladores/Manipular-fps.cpp
@@ -6,7 +6,6 @@
 #include <chrono>
 namespace fs=std::filesystem;
 
-// pasta do free fire, se mudar o jogo troca aqui
 std::string DIR_FF="storage/shared/Android/data/com.freefireth/files";
 
 // função que troca um valor por outro dentro do texto do arquivo
